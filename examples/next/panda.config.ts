a9d68a8f9fb4557c847ace6ce396bf62dd9b8944
togana <o.togana@gmail.com>
2024-06-15 21:59:39 +0900

    next にpreset いれるだけの example 書いた

---
+++ examples/next/panda.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from "@pandacss/dev";
+import { preset } from '../../packages/digital-go-jp-panda-preset/src/index';
+
+export default defineConfig({
+  // Whether to use css reset
+  preflight: true,
+
+  presets: [preset],
+
+  // Where to look for your css declarations
+  include: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
+
+  // Files to exclude
+  exclude: [],
+
+  // Useful for theme customization
+  theme: {
+    extend: {
+      tokens: {
+        // next のフォントを利用するために変数を読み込む
+        fonts: {
+          body: {
+            value: "var(--font-noto-sans-jp), -apple-system, BlinkMacSystemFont, sans-serif",
+          },
+          code: {
+            value: "var(--font-noto-sans-mono), monospace",
+          },
+        }
+      }
+    },
+  },
+
+  // The output directory for your css system
+  outdir: "styled-system",
+});
