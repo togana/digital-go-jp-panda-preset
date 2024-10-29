c0c13a6170921342de9c6eeb5f18bee955301405
togana <o.togana@gmail.com>
2024-10-29 23:16:11 +0900

    存在しなくなった textStyle を参照していた

--- examples/next/src/app/page.tsx
+++ examples/next/src/app/page.tsx
@@ -3,11 +3,11 @@ import { css } from '@/../styled-system/css';
 export default function Home() {
   return (
     <>
-      <div className={css({ textStyle: 'Dsp-64N-4', backgroundColor: 'success-1' })}>Hello 🐼!</div>
-      <div className={css({ textStyle: 'Dsp-57N-4', backgroundColor: 'error-1' })}>Hello 🐼!</div>
-      <div className={css({ textStyle: 'Dsp-48N-4', backgroundColor: 'warning-yellow-1' })}>Hello 🐼!</div>
-      <div className={css({ textStyle: 'Mono-14N-5', backgroundColor: 'warning-yellow-1' })}>Hello 🐼!</div>
-      <div className={css({ textStyle: 'Mono-14B-5', backgroundColor: 'warning-yellow-1' })}>Hello 🐼!</div>
+      <div className={css({ textStyle: 'Dsp-64N-140', backgroundColor: 'success-1' })}>Hello 🐼!</div>
+      <div className={css({ textStyle: 'Dsp-57N-140', backgroundColor: 'error-1' })}>Hello 🐼!</div>
+      <div className={css({ textStyle: 'Dsp-48N-140', backgroundColor: 'warning-yellow-1' })}>Hello 🐼!</div>
+      <div className={css({ textStyle: 'Mono-14N-150', backgroundColor: 'warning-yellow-1' })}>Hello 🐼!</div>
+      <div className={css({ textStyle: 'Mono-14B-150', backgroundColor: 'warning-yellow-1' })}>Hello 🐼!</div>
     </>
   )
 }
