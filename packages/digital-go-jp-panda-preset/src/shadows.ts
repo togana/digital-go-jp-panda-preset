2d3e4046a4698572a83e64729669c0f091431d6f
togana <o.togana@gmail.com>
2025-01-25 01:18:16 +0900

    Shadow トークンの名称変更

--- packages/digital-go-jp-panda-preset/src/shadows.ts
+++ packages/digital-go-jp-panda-preset/src/shadows.ts
@@ -3,12 +3,12 @@ import type { Tokens } from '@pandacss/types';
 
 // https://design.digital.go.jp/foundations/elevation/
 export const shadows: Tokens['shadows'] = {
-  1: { value: tokens.Shadow[1].$value },
-  2: { value: tokens.Shadow[2].$value },
-  3: { value: tokens.Shadow[3].$value },
-  4: { value: tokens.Shadow[4].$value },
-  5: { value: tokens.Shadow[5].$value },
-  6: { value: tokens.Shadow[6].$value },
-  7: { value: tokens.Shadow[7].$value },
-  8: { value: tokens.Shadow[8].$value },
+  1: { value: tokens.Elevation[1].$value },
+  2: { value: tokens.Elevation[2].$value },
+  3: { value: tokens.Elevation[3].$value },
+  4: { value: tokens.Elevation[4].$value },
+  5: { value: tokens.Elevation[5].$value },
+  6: { value: tokens.Elevation[6].$value },
+  7: { value: tokens.Elevation[7].$value },
+  8: { value: tokens.Elevation[8].$value },
 };
