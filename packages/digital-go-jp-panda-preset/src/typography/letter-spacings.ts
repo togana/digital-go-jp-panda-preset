c7c011b8dec1fdcbd1cab6f863c2a02c01599cca
togana <o.togana@gmail.com>
2025-08-30 16:07:37 +0900

    typographyのletterSpacingsとlineHeightsの定義を更新

--- packages/digital-go-jp-panda-preset/src/typography/letter-spacings.ts
+++ packages/digital-go-jp-panda-preset/src/typography/letter-spacings.ts
@@ -4,5 +4,6 @@ import type { Tokens } from '@pandacss/types';
 // https://design.digital.go.jp/foundations/typography/
 export const letterSpacings: Tokens['letterSpacings'] = {
   0: { value: '0' },
+  0.01: { value: '0.01em' },
   0.02: { value: '0.02em' },
 };
--- packages/digital-go-jp-panda-preset/src/typography/line-heights.ts
+++ packages/digital-go-jp-panda-preset/src/typography/line-heights.ts
@@ -3,12 +3,12 @@ import type { Tokens } from '@pandacss/types';
 
 // https://design.digital.go.jp/foundations/typography/#h3-%E8%A1%8C%E3%83%9C%E3%83%83%E3%82%AF%E3%82%B9%E3%81%AE%E9%AB%98%E3%81%95%EF%BC%88line-height%EF%BC%89
 export const lineHeights: Tokens['lineHeights'] = {
-  1: { value: tokens.LineHeight['100'].$value },
-  2: { value: tokens.LineHeight['120'].$value },
-  3: { value: tokens.LineHeight['130'].$value },
-  4: { value: tokens.LineHeight['140'].$value },
-  5: { value: tokens.LineHeight['150'].$value },
-  6: { value: tokens.LineHeight['160'].$value },
-  7: { value: tokens.LineHeight['170'].$value },
-  8: { value: tokens.LineHeight['175'].$value },
+  100: { value: tokens.LineHeight['100'].$value },
+  120: { value: tokens.LineHeight['120'].$value },
+  130: { value: tokens.LineHeight['130'].$value },
+  140: { value: tokens.LineHeight['140'].$value },
+  150: { value: tokens.LineHeight['150'].$value },
+  160: { value: tokens.LineHeight['160'].$value },
+  170: { value: tokens.LineHeight['170'].$value },
+  175: { value: tokens.LineHeight['175'].$value },
 };
